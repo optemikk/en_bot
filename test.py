99b9927488a5a6329c15ad8242289ea9fd3a9ad0
unknown <jonny0685@gmail.com>
2023-09-16 18:33:43 +0400

    release1

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-token = '6524572114:AAG4WfvdNkswheVRkIjg4ir0BWLc-MLgvKs'
-chat_id = '972383332'
-url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}>&text=Всем привет!'
-response = requests.get(url=url)
-print(response.json())
